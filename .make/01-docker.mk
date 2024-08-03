e8f184794082fbebb70fa311ef2665b116a96f2e
Stephane Berube <sirber83@gmail.com>
2024-08-03 11:49:06 -0400

    added: Makefole start/stop

--- .make/01-docker.mk
+++ .make/01-docker.mk
@@ -20,14 +20,22 @@ build:
 	@$(COMPOSE_CMD) build
 
 .PHONY: up
-up: build ## Build and Start the project
+up: ## Ups the container
 	@$(COMPOSE_CMD) up -d
 	@$(COMPOSE_CMD) exec php composer install --no-progress
 
 .PHONY: down
-down: ## Stop the project
+down: ## Downs the container
 	@$(COMPOSE_CMD) down --remove-orphans
 
+.PHONY: start
+start: ## Starts the container
+	@$(COMPOSE_CMD) start
+
+.PHONY: stop
+stop: ## Stops the container
+	@$(COMPOSE_CMD) stop
+
 .PHONY: cli
-cli: ## Launch a cli in the container
+cli: ## Launch a cli inside the container
 	@$(COMPOSE_CMD) exec -it php sh
