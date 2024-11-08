14fd6d4f79db5c90e0916654fdd46d8afcabcf92
Stephane Berube <sirber83@gmail.com>
2024-11-07 21:15:12 -0500

    cleanup

--- .make/02-application.mk
+++ .make/02-application.mk
@@ -3,4 +3,7 @@
 quality: ## Cleanup the project
 	@docker compose exec php /app/vendor/bin/phpstan analyze
 	@docker compose exec php /app/vendor/bin/php-cs-fixer fix src
+
+.PHONY: test
+test: ## Run tests
 	@docker compose exec php /app/vendor/bin/phpunit ./tests
