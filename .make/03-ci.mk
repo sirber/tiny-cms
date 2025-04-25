aa9261ddc7ed992e9946491a34feaebdea0e7d7e
Stéphane Bérubé <sirber83@gmail.com>
2025-04-25 19:20:59 -0400

    fixed: ciquality

--- .make/03-ci.mk
+++ .make/03-ci.mk
@@ -2,7 +2,7 @@
 .PHONY: ci-quality
 ciquality: ## Cleanup the project
 	@php ./vendor/bin/phpstan analyze
-	@php ./vendor/bin/php-cs-fixer src
+	@php ./vendor/bin/php-cs-fixer check src
 
 .PHONY: test
 citest: ## Run tests
